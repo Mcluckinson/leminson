08652137528fb35a06fd191dfe3bcc57277be729
Vladislav <clothor-@student.21-school.ru>
2020-08-23 20:57:02 +0300

    TODO: School is closing

--- sources/power_levels.c
+++ sources/power_levels.c
@@ -10,11 +10,42 @@ static void set_known_levels(t_main *map)
     map->end->level = END;
 }
 
+static void set_level(t_room *level, t_room *level2)
+{
+    if (level2 < level && level2 != 0)
+    {
+        level = level2;
+    }
+    else if (level == 0)
+    {
+        level = 1;
+    }
+}
+
 bool power_levels(t_main *map)
 {
     set_known_levels(map);
-    t_link *link = map->all_links_here;
-    t_room *room = map->all_rooms_here;
+    t_room *room = map->all_rooms_here->next;
+    while (room->next)
+    {
+        t_link *link = map->all_links_here;
+        while (link->next)
+        {
+            if (room->name == link->first_room->name)
+            {
+//                room->level = room->level + 1;
+                set_level(room, link->first_room);
+            }
+            else if (room->name == link->second_room->name)
+            {
+//                room->level = room->level + 1;
+                set_level(&room->level, &link->second_room->level);
+            }
+            link = link->next;
+        }
+        room = room->next;
+    }
+    printf("Ok\n");
 
 //    link->first_room
 
